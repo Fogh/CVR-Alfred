64c3327b5e739669d4f1e060c94e560769d6f28b
Anders Eriksen <andfogh@gmail.com>
2013-06-18 18:59:12 +0200

    Yay! Its now possible to lookup company by the name. Resolves #1

--- source/cvr.py
+++ source/cvr.py
@@ -1,35 +1,47 @@
 import json
 import urllib2
+import urllib
 from feedback import Feedback
 
 _DEFAULTHOST = "https://cvrapi.dk/"
 
 
-def url(cvr):
-    return _DEFAULTHOST + cvr
+def get_data(cvr="", name=""):
+    if cvr:
+        req = urllib2.Request(_DEFAULTHOST + cvr)
+    elif name:
+        req = urllib2.Request(_DEFAULTHOST + "name/" + urllib.quote(name))
+    req.add_header("Accept", "application/json")
+    req.add_header("User-agent", "CVR-Alfred")
+    try:
+        res = urllib2.urlopen(req)
+        return json.loads(res.read())
+    except urllib2.URLError:
+        print "Kan ikke oprette forbindelse"
+        raise SystemExit()
 
 
-def get_data(cvr):
+def parse_data(data, showCVR):
     fb = Feedback()
-    if len(cvr) == 8:
-        req = urllib2.Request(url(cvr))
-        req.add_header("Accept", "application/json")
-        req.add_header("User-agent", "CVR-Alfred")
-        try:
-            res = urllib2.urlopen(req)
-        except urllib2.URLError:
-            print "Kan ikke oprette forbindelse"
-            raise SystemExit()
+    address = data['adresse'] + ", " + str(data['postnr']) + " " + data['by']
+    name = data['navn']
+    if showCVR:
+        name = data['navn'] + " - " + str(data['cvr'])
+    fb.add_item(name, address + " - Tlf: " + data['telefon'], str(data['cvr']))
+    return fb
 
-        data = json.loads(res.read())
 
+def lookup(query):
+    fb = Feedback()
+    if len(query) == 8 and query.isdigit():
+        data = get_data(query)
         if 'error' in data:
             fb.add_item("Ugyldigt CVR-nummer")
         else:
-            address = data['adresse'] + ", " + str(data['postnr']) + " " + data['by']
-            fb.add_item(data['navn'],  address + " - Tlf: " + data['telefon'], cvr)
-    elif len(cvr) > 8:
+            fb = parse_data(data, False)
+    elif len(query) > 8 and query.isdigit():
         fb.add_item("CVR-nummeret er for langt", "CVR-numre er 8-cifret")
     else:
-        fb.add_item("Indtast CVR-nummer")
+        data = get_data(name=query)
+        fb = parse_data(data, True)
     print fb
